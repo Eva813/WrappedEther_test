2b8cb9b3c1b326e806c7798895de3b6dac1309d8
Eva <as45986@gmail.com>
2023-10-10 20:14:58 +0800

    feat:add wrappedEther file

---
+++ src/WrappedEther.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WETH is ERC20{
+    constructor() ERC20("Wrapped ETH", "WETH") {}
+
+    event LogAddressBalance(uint _balance);
+
+    function depositWETH() public  payable {
+        _mint(msg.sender,msg.value);
+        //透過event 顯示餘額
+        emit LogAddressBalance(address(this).balance);
+    }
+
+    fallback() external payable {
+        depositWETH();
+    }
+
+    receive() external payable {}
+
+    function unWrapped(uint _amount) external  payable {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+        _burn(msg.sender, _amount);
+        //要將錢還給該帳戶
+        (bool success,) = msg.sender.call{value: _amount}("");
+        require(success,"fail to unwrap");
+    }
+}
